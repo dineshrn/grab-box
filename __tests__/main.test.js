4a0a9be09bc5881707c7c8a7f4c3be1792705553
Dinesh R N <rndinesh751@gmail.com>
2024-11-01 14:33:09 +0530

    feat: log version

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,6 +1,10 @@
 const { execSync } = require("child_process");
+const packageJson = require("../package.json"); //with { type: "json" };
 
 test("greet command", () => {
   const output = execSync("node index.js").toString();
-  expect(output).toMatch("GRAB BOX - WIP - Under Development");
+  expect(output).toMatch(
+    "GRAB BOX - WIP - Under Development",
+    packageJson.version
+  );
 });
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import packageJson from "./package.json" with { type: "json" };
+
 function main() {
-  console.log("GRAB BOX - WIP - Under Development");
+  console.log("GRAB BOX - WIP - Under Development", packageJson.version);
 }
 
 main();
